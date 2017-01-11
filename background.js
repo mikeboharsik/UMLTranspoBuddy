18f54c31009503f803cf37be208cf3745246ed4c
ganonscrub <mboharsik@gmail.com>
2017-01-11 02:06:32 -0500

    Updated syntax

--- background.js
+++ background.js
@@ -4,9 +4,9 @@ var URLs = {
 };
 
 function addEvent( queryData ){
-	chrome.storage.local.get( "calendar", function( item ){
+	chrome.storage.local.get( 'calendar', ( item ) => {
 		if ( item.calendar ){
-			chrome.identity.getAuthToken( { 'interactive': true }, function(token){
+			chrome.identity.getAuthToken( { 'interactive': true }, (token) => {
 				$.ajax({
 					url: URLs.addEvent.replace( "{calendarID}", item.calendar.id ),
 					type: 'POST',
@@ -14,30 +14,30 @@ function addEvent( queryData ){
 					contentType: 'application/json; charset=utf-8',
 					dataType: 'json',
 					headers: { 'Authorization': 'Bearer ' + token },
-					success: function( response ){
-						chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-							chrome.tabs.sendMessage(tabs[0].id, { success: true }, function(response) {
+					success: ( response ) => {
+						chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+							chrome.tabs.sendMessage(tabs[0].id, { success: true }, (response) => {
 								//console.log(response.farewell);
 							});
 						});
 					},
-					error: function( response ) { console.log( "error:", response ); }
+					error: ( response ) => { console.log( 'error:', response ); }
 				});
 			});
 		}
 		else{
-			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-				chrome.tabs.sendMessage(tabs[0].id, { error: "calendar not set" }, function(response) {
+			chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+				chrome.tabs.sendMessage(tabs[0].id, { error: 'calendar not set' }, (response) => {
 					//console.log(response.farewell);
 				});
 			});
-			console.error( "Couldn't get calendar" );
+			console.error( 'Could not get calendar' );
 		}
 	});
 }
 
 function getCalendarEvents(){
-	chrome.storage.local.get( "calendar", function( item ){
+	chrome.storage.local.get( 'calendar', ( item ) => {
 		if ( item.calendar ){
 			var eventsURL = 'https://www.googleapis.com/calendar/v3/calendars/' + item.calendar.id + '/events?';
 			
@@ -50,18 +50,18 @@ function getCalendarEvents(){
 			eventsURL += 'timeMax=' + max.toISOString() + '&';
 			eventsURL += 'timeZone=America/New_York';
 			
-			console.log( "Running thing..." );
+			console.log( 'Running thing...' );
 			
-			chrome.identity.getAuthToken( { 'interactive': true }, function(token){
+			chrome.identity.getAuthToken( { 'interactive': true }, (token) => {
 				$.ajax({
 					url: eventsURL,
 					type: 'GET',
 					headers: { 'Authorization': 'Bearer ' + token },
-					success: function( resp ){
+					success: ( resp ) => {
 						var startEnds = [];
 						for ( i of resp.items ){
-							if ( i.status == "cancelled" )
-								console.info( "Ignoring cancelled event:", i );
+							if ( i.status == 'cancelled' )
+								console.info( 'Ignoring cancelled event:', i );
 							else
 								startEnds.push( { start: i.start.dateTime, end: i.end.dateTime } );
 						}
@@ -79,26 +79,29 @@ function openOptionsTab(){
 }
 
 chrome.runtime.onMessage.addListener(
-	function( request, sender, sendResponse ) {
+	( request, sender, sendResponse ) => {
 		//console.log( request );
 		switch ( request.type ){
-		case 'addEvent':
-			if ( request.data ){
-				addEvent( request.data );
-				sendResponse( { type: request.type } );
-			}
-			else
-				sendResponse( { type: request.type, error: 'No data property supplied in request' } );
-			break;
-		case 'openOptionsTab':
-			openOptionsTab();
-			break;
-		case 'getCalendarEvents':
-			getCalendarEvents();
-			sendResponse( { type: request.type, message: 'Attempted to get events' } );
-			break;
-		default:
-			sendResponse( { type: request.type, error: 'Unhandled request type given' } );
+			case 'addEvent':
+				if ( request.data ){
+					addEvent( request.data );
+					sendResponse( { type: request.type } );
+				}
+				else
+					sendResponse( { type: request.type, error: 'No data property supplied in request' } );
+				break;
+				
+			case 'openOptionsTab':
+				openOptionsTab();
+				break;
+				
+			case 'getCalendarEvents':
+				getCalendarEvents();
+				sendResponse( { type: request.type, message: 'Attempted to get events' } );
+				break;
+				
+			default:
+				sendResponse( { type: request.type, error: 'Unhandled request type given' } );
 		}
 	}
 );
